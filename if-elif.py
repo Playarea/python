25102ddda6abe5b8f7a86406af8e15cced316d9b
Playarea <Subram.Nagulapalli@gmail.com>
2026-05-01 22:38:19 -0500

    using if-elif with input

---
+++ if-elif.py
@@ -0,0 +1,17 @@
+print("1 - Good Morning")
+print("2 - Good Evening")
+print("3 - Good Afternoon")
+print("4 - Good Night")
+
+choice = int (input("Enter your choice: "))
+if choice == 1:
+    print("Good Morning Subram")
+elif choice == 2:
+    print("Good Evening Subram")
+elif choice == 3:
+    print("Good Afternoon Subram")
+elif choice == 4:
+    print("Good Night Subram")
+else:
+    print("Invalid Choice")
+
